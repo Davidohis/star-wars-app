104e233e9b9566f528ec0d47782e5e418f8774f4
David Ohis <davidi.ohis25@gmail.com>
2022-09-22 15:27:36 +0100

    star wars app

--- src/components/organisms/Preview/index.tsx
+++ src/components/organisms/Preview/index.tsx
@@ -1,21 +1,26 @@
-import { Typography } from 'antd';
-import { PreviewContents, PreviewWrapper } from './styles'
+import { Typography } from "antd";
+import { PreviewContents, PreviewWrapper } from "./styles";
 import * as Atom from "../../atoms";
 import Image from "../../../assets/preveiw-img.png";
 
-export default function PreviewComponent() {
-    const { Title, Paragraph } = Typography;
+export default function PreviewComponent({ title }: any) {
+  const { Title, Paragraph } = Typography;
 
   return (
     <PreviewWrapper>
-       <Atom.ImageComponent image={Image} alt={"logo"} sx={"318px"} hg={"height"} />
-
-       <PreviewContents>
-       <Title>COVER</Title>
-       <Paragraph>Director: Kingsley Omin</Paragraph>
-       <Paragraph>Producer: Kingsley Omin</Paragraph>
-       <Paragraph>Release Date: January 24, 2022.</Paragraph>
-       </PreviewContents>
+      <Atom.ImageComponent
+        image={Image}
+        alt={"logo"}
+        sx={"318px"}
+        hg={"height"}
+      />
+      <PreviewContents>
+        <Title>{title}</Title>
+        <Paragraph>Director: Kingsley Omin</Paragraph>
+        <Paragraph>Producer: Kingsley Omin</Paragraph>
+        <Paragraph>Release Date: January 24, 2022.</Paragraph>
+        <Paragraph>No Time </Paragraph>
+      </PreviewContents>
     </PreviewWrapper>
-  )
+  );
 }
--- src/components/pages/Preview/index.tsx
+++ src/components/pages/Preview/index.tsx
@@ -1,13 +1,17 @@
 import * as Organism from "../../organisms";
 import { Layout } from "../../templates";
 import { GlobalContainerWrapper } from "../styles";
+import { useParams } from "react-router-dom";
 
 export default function PreviewPage() {
+  const params = useParams();
+  const title = params.id;
+
   return (
     <Layout>
       <GlobalContainerWrapper>
         <Organism.HeaderComponent goback={true} />
-        <Organism.PreviewComponent />
+        <Organism.PreviewComponent title={title} />
         <Organism.SiderBarComponent />
       </GlobalContainerWrapper>
     </Layout>
